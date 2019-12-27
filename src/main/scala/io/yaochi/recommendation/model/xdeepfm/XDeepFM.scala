a972d174954f9df82ed1ff39cd999e5007b70785
yaochi <yaochi@sugo.io>
2019-12-27 19:17:50 +0800

    update xdeepfm module

--- src/main/scala/io/yaochi/recommendation/model/xdeepfm/XDeepFM.scala
+++ src/main/scala/io/yaochi/recommendation/model/xdeepfm/XDeepFM.scala
@@ -5,6 +5,8 @@ import io.yaochi.recommendation.model.{RecModel, RecModelType}
 class XDeepFM(inputDim: Int, nFields: Int, embeddingDim: Int, fcDims: Array[Int], cinDims: Array[Int])
   extends RecModel(RecModelType.BIAS_WEIGHT_EMBEDDING_MATS) {
 
+  private val innerModel = new InternalXDeepFMModel(nFields, embeddingDim, fcDims, cinDims)
+
   override def getMatsSize: Array[Int] = {
     val dims = Array(nFields * embeddingDim) ++ fcDims ++ Array(1)
     (1 until dims.length)
@@ -17,10 +19,49 @@ class XDeepFM(inputDim: Int, nFields: Int, embeddingDim: Int, fcDims: Array[Int]
   override def getEmbeddingDim: Int = embeddingDim
 
   override protected def forward(params: Map[String, Any]): Array[Float] = {
-    null
+    val batchSize = params("batch_size").asInstanceOf[Int]
+    val index = params("index").asInstanceOf[Array[Long]].map(_.toInt)
+    val weights = params("weights").asInstanceOf[Array[Float]]
+    val bias = params("bias").asInstanceOf[Array[Float]]
+    val embedding = params("embedding").asInstanceOf[Array[Float]]
+    val mats = params("mats").asInstanceOf[Array[Float]]
+
+    innerModel.forward(batchSize, index, weights, bias, embedding, mats)
   }
 
   override protected def backward(params: Map[String, Any]): Float = {
+    val batchSize = params("batch_size").asInstanceOf[Int]
+    val index = params("index").asInstanceOf[Array[Long]].map(_.toInt)
+    val weights = params("weights").asInstanceOf[Array[Float]]
+    val bias = params("bias").asInstanceOf[Array[Float]]
+    val embedding = params("embedding").asInstanceOf[Array[Float]]
+    val mats = params("mats").asInstanceOf[Array[Float]]
+    val targets = params("targets").asInstanceOf[Array[Float]]
+
+    innerModel.backward(batchSize, index, weights, bias, embedding, mats, targets)
+  }
+}
+
+private[xdeepfm] class InternalXDeepFMModel(nFields: Int,
+                                           embeddingDim: Int,
+                                           fcDims: Array[Int],
+                                           cinDim: Array[Int]) {
+  def forward(batchSize: Int,
+              index: Array[Int],
+              weights: Array[Float],
+              bias: Array[Float],
+              embedding: Array[Float],
+              mats: Array[Float]): Array[Float] = {
+    null
+  }
+
+  def backward(batchSize: Int,
+               index: Array[Int],
+               weights: Array[Float],
+               bias: Array[Float],
+               embedding: Array[Float],
+               mats: Array[Float],
+               targets: Array[Float]): Float = {
     0f
   }
 }
