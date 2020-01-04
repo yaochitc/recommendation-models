9e3127449c917f58095d551f488ef43cdcf8996d
yaochi <yaochi@sugo.io>
2020-01-04 13:29:00 +0800

    add a offset parameter

--- src/main/scala/io/yaochi/recommendation/model/encoder/HigherOrderEncoder.scala
+++ src/main/scala/io/yaochi/recommendation/model/encoder/HigherOrderEncoder.scala
@@ -10,7 +10,8 @@ class HigherOrderEncoder(batchSize: Int,
                          nFields: Int,
                          embeddingDim: Int,
                          fcDims: Array[Int],
-                         mats: Array[Float]) {
+                         mats: Array[Float],
+                         start: Int = 0) {
   private val linearLayers = buildLinearLayers()
   private val module = buildModule()
 
@@ -20,7 +21,7 @@ class HigherOrderEncoder(batchSize: Int,
 
   def backward(input: Tensor[Float], gradOutput: Tensor[Float]): Tensor[Float] = {
     val gradTensor = module.backward(input, gradOutput).toTensor[Float]
-    var curOffset = 0
+    var curOffset = start
     for (linearLayer <- linearLayers) {
       val gradWeight = linearLayer.gradWeight
       val gradBias = linearLayer.gradBias
@@ -56,7 +57,7 @@ class HigherOrderEncoder(batchSize: Int,
 
   private def buildLinearLayers(): Array[Linear[Float]] = {
     val layers = ArrayBuffer[Linear[Float]]()
-    var curOffset = 0
+    var curOffset = start
     var dim = nFields * embeddingDim
     for (fcDim <- fcDims) {
       layers += LayerUtil.buildLinear(dim, fcDim, mats, curOffset)
@@ -73,6 +74,7 @@ object HigherOrderEncoder {
             nFields: Int,
             embeddingDim: Int,
             fcDims: Array[Int],
-            mats: Array[Float]): HigherOrderEncoder =
-    new HigherOrderEncoder(batchSize, nFields, embeddingDim, fcDims, mats)
+            mats: Array[Float],
+            start: Int = 0): HigherOrderEncoder =
+    new HigherOrderEncoder(batchSize, nFields, embeddingDim, fcDims, mats, start)
 }
